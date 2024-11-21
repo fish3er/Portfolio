7030320ea8eee89054d5211297d32c72fb0f483d
Fisher <mateuszrybak631@gmail.com>
2024-11-21 16:04:48 +0100

    dodano bubblesort

---
+++ Sortowania/src/BubbleSortExample.java
@@ -0,0 +1,24 @@
+public class BubbleSortExample {
+    public static void bubbleSort(int[] arr) {
+       int count = arr.length; // ilość przejść
+       while (count > 1) { // prawdopodobnie najmiejsza możliwa ilość przejść alby skoczyć
+           for (int i = 0; i < arr.length - 1; i++) { // tylko jedno przejście
+               if (arr[i] > arr[i + 1]) {
+                   int temp = arr[i];
+                   arr[i] = arr[i + 1];
+                   arr[i + 1] = temp;
+               }
+               ;
+
+           }
+           count--;
+       }
+    }
+    public static void main(String[] args) {
+        int[] myArray = {10, 9, 8,7,6,5,4,3,2,1};
+        bubbleSort(myArray);
+        for (int i : myArray){
+            System.out.print(i);
+        }
+    }
+}
